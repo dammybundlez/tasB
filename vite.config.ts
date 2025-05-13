12c06431797e0d5c87f703975fe5ca806d584bd4
OguikeGodswill <108469994+dammybundlez@users.noreply.github.com>
2025-05-13 08:39:31 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct asset loading on Vercel and other static hosts
+  base: '/', // Ensures correct asset loading on Vercel and other static hosts
   server: {
     host: '0.0.0.0',
     port: 5173
