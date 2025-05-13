d3426f7feff7565bf286afc256cd93038781215b
OguikeGodswill <108469994+dammybundlez@users.noreply.github.com>
2025-05-13 08:13:39 -0700

    Update RecipeGrid.tsx

--- src/pages/RecipeGrid.tsx
+++ src/pages/RecipeGrid.tsx
@@ -65,7 +65,8 @@ const RecipeGrid = () => {
       </div>
 
       <div>
-      <p className="flex justify-center items-center rounded-md p-1 text-center ">Page - {page}</p>
+              <p className="text-center text-pink-600 font-bold mb-5 flex justify-center">Page - {page}</p>
+
          {loading && 
          <div role="status" className="flex justify-center items-center">
             <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
